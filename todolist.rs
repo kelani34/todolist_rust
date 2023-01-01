892eeb1aed4131f7468abf9ac278c8235fb3365d
kelani34 <kelanitaiwo34@gmail.com>
2023-01-01 16:23:30 +0100

    add: main

--- todolist.rs
+++ todolist.rs
@@ -34,3 +34,43 @@ impl TodoList {
         }
     }
 }
+
+fn main() {
+    let mut todo_list = TodoList::new();
+
+    loop {
+        println!("1. Add task");
+        println!("2. Mark task complete");
+        println!("3. Print tasks");
+        println!("4. Quit");
+
+        let mut choice = String::new();
+        io::stdin().read_line(&mut choice).expect("Failed to read line");
+        let choice: u32 = match choice.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match choice {
+            1 => {
+                println!("Enter task name:");
+                let mut task_name = String::new();
+                io::stdin().read_line(&mut task_name).expect("Failed to read line");
+                let task = Task {
+                    name: task_name,
+                    completed: false,
+                };
+                todo_list.add_task(task);
+            }
+            2 => {
+                println!("Enter task name:");
+                let mut task_name = String::new();
+                io::stdin().read_line(&mut task_name).expect("Failed to read line");
+                todo_list.mark_task_complete(task_name.trim());
+            }
+            3 => todo_list.print_tasks(),
+            4 => break,
+            _ => continue,
+        }
+    }
+}
